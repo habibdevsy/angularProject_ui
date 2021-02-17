e7f114416e795f190f28519d018d10d9aee531f1
habibdevsy <habibdevsy>
2021-02-16 21:25:59 -0800

    commit first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {TitleComponent} from './title/title.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { TitleComponent } from './title/title.component';
+import { NotificationComponent } from './notification/notification.component';
+import { PhotoComponent } from './photo/photo.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TitleComponent,
+    NotificationComponent,
+    PhotoComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
