2db77f31910631724b42e811edc349f7f4b387b6
habibdevsy <habibdevsy>
2021-03-03 00:09:17 -0800

    update tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import {TitleComponent} from './title/title.component';
 })
 export class AppComponent {
   title = 'pro';
+  active = 0;
+  tab1 = 1;
+  tab2 = 0;
+  tab3 = 0;
+
+  show(n:number) {
+    if (n == 1) { 
+      this.tab1 = 1;
+      this.tab2 = 0;
+      this.tab3 = 0;
+    }
+    if (n == 2) { 
+      this.tab1 = 0;
+      this.tab2 = 1;
+      this.tab3 = 0;
+    }
+    if (n == 3) { 
+      this.tab1 = 0;
+      this.tab2 = 0;
+      this.tab3 = 1;
+    }
+
+  }
 }
